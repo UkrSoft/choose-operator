5642877caf6ccb4657663f6a13324e21e995e60e
Dan Goriaynov <dan.goriaynov@gmail.com>
2015-08-09 17:01:46 -0600

    now no need to create superuser manually after each dump import - it is done automatically (I think there should be also an option to look better on what django password hash depends on...)

--- _dump-import.cmd
+++ _dump-import.cmd
@@ -7,6 +7,9 @@ set dump_location=%current_location%dump\
 set utils_location=%dump_location%utils\
 set back_db_file=back.data
 set full_dump_path=%dump_location%%back_db_file%
+echo Are you READY for the show?
+pause >nul
+
 echo Extracting dump file...
 "%utils_location%7z.exe" x "%full_dump_path%.zip" -o"%dump_location%" >nul
 if errorlevel 1 (
@@ -14,6 +17,15 @@ goto :exit_label
 ) else (
 echo OK.
 )
+
+echo Flusing the database...
+mysql -b -u%username% -p%pass% < _mysql_recreate.sql
+if errorlevel 1 (
+goto :exit_label
+) else (
+echo OK.
+)
+
 echo Importing dump file into database...
 mysql -u%username% -p%pass% --database %db_name%<"%full_dump_path%"
 if errorlevel 1 (
@@ -21,6 +33,15 @@ goto :exit_label
 ) else (
 echo OK.
 )
+
+echo Re-creating superuser (admin)
+echo from django.contrib.auth.models import User; u,c = User.objects.get_or_create(username='admin'); u.set_password('111'); u.save(); | python manage.py shell >nul
+if errorlevel 1 (
+goto :exit_label
+) else (
+echo OK.
+)
+
 echo Deleting dump file...
 del "%full_dump_path%"
 if errorlevel 1 (
@@ -28,8 +49,10 @@ goto :exit_label
 ) else (
 echo OK.
 )
-pause
+echo Finished! Be creative and push ANY key.
+pause >nul
 exit
+
 :exit_label
 echo Operation FAILED!
 pause
