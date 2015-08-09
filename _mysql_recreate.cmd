ee085edce7266214ab51d61afa6ff702c09301d2
Kostiantyn Bezverkhyi <mailtendzin@gmail.com>
2015-08-09 23:47:50 +0200

    Minor changes of help_text in model

--- _mysql_recreate.cmd
+++ _mysql_recreate.cmd
@@ -3,6 +3,6 @@ echo This will DROP existing DB and create it from the scratch!!!
 echo.
 echo ARE YOU SURE?
 pause
-mysql -b -u root -p1111 bestoperator < recreate.sql
+mysql -b -u root -p1111 < _mysql_recreate.sql
 echo DONE!
 pause
