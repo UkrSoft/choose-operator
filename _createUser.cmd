952a2947fa98d8c7d968832ba7e1798e330b612a
Dan Goriaynov <dan.goriaynov@gmail.com>
2015-08-09 11:21:01 -0600

    fixed createsuperuser

--- _createUser.cmd
+++ _createUser.cmd
@@ -1,4 +1,4 @@
 set username="admin"
 set email="admin@example.com"
-%python% manage.py createsuperuser --username=%username% --email=%email%
+python manage.py createsuperuser --username=%username% --email=%email%
 pause
